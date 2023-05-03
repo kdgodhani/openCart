59fa18d770b339cc1b9cf0e9c0ca602970dd5382
kashyap patel <patelkashyap599@gmail.com>
2023-05-04 00:04:58 +0530

    modify user route name

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const usersRouter = require('./users');
+const usersRouter = require('./user.route');
 /* GET home page. */
 router.use('/user',usersRouter );
 
